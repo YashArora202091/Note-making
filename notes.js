e55835ed364263bfdb161152e21c4198002984a7
YashArora202091 <63581376+YashArora202091@users.noreply.github.com>
2022-09-30 18:48:28 +0530

    Add files via upload

---
+++ notes.js
@@ -0,0 +1,25 @@
+const notes= [
+    {
+        key:1,
+        title:"Delegation",
+        content:"Q.How many programmers does it take to change a bulb? Ans:None,that is a hardware problem"
+    },
+    {
+        key:2,
+        title:"Loops",
+        content:"Q.How to keep a programmer forever in a shower room? Ans:Show him the shampoo bottle with instructions lather,rinse,repeat."
+    },
+    {
+        key:3,
+        title:"Arrays",
+        content:"Q.Why did the programmer quit the job? Ans:Because he didn't get the arrays."
+    },
+    {
+        key:4,
+        title:"Hardware vs Software",
+        content:"You can hit your hardware with a hammer but only curse at your software."
+    }
+
+];
+
+export default notes;
